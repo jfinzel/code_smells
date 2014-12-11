369f8d9d0bd0ea9026423af133088838dadb0a99
Daniel Schepers <Daniel@dan-on-mercury.enova.com>
2014-12-11 14:10:35 -0600

    ready for round 2

--- ruby/assault_course/customer.rb
+++ ruby/assault_course/customer.rb
@@ -81,18 +81,4 @@ class Customer
     result
   end
 
-  private
-  def age_in_years
-      today = Date.today
-      date_of_birth = Date.strptime(@date_of_birth, "%Y-%m-%d")
-
-      age_year = today.year - date_of_birth.year
-      age_month = today.month - date_of_birth.month
-
-      age_year -= negative_month_difference(age_month)
-  end
-
-  def negative_month_difference(months)
-    months < 0 ? 1 : 0
-  end
 end
